7740ed870ebaa29ada4120046b54657ffc1b5f8f
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-04-03 21:59:07 +0900

    Update Alternately.py

--- Alternately.py
+++ Alternately.py
@@ -1,10 +1,19 @@
 def alternately(n,s):
     for i in range(0,n+1):
-        if ((i%2==0)and(s[i] == 'M')) and ((i%2==1)and(s[i] == 'F')):
+        if ((s[2*i] == 'M')) and ((s[2*i+1] == 'F')):
             return 'Yes'
         else:
             return 'No'
 
+"""N=int(input())
+S=input()
+for i in range(N-1):
+  if S[i]==S[i+1]:
+    print("No")
+    exit()
+print("Yes")"""
+
 print(alternately(6,'MFMFMF'))
 print(alternately(9,'FMFMMFMFM'))
 print(alternately(1,'F'))
+
