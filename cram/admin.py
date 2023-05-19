b6e546dfd4d58597a4d0a329b352869e76ea2ddc
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-05-19 21:35:11 +0900

    追加

--- cram/admin.py
+++ cram/admin.py
@@ -3,19 +3,33 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .models import Article
+from .models import Article, Comment
 
 class ArticleResource(resources.ModelResource):
     # Modelに対するdjango-import-exportの設定
     class Meta:
         model = Article
 
+class CommentResource(resources.ModelResource):
+    # Modelに対するdjango-import-exportの設定
+    class Meta:
+        model = Comment
+
 
 @admin.register(Article)
 class ArticleAdmin(ImportExportModelAdmin):
     # ImportExportModelAdminを利用するようにする
     ordering = ['id']
-    list_display = ('id', 'title', 'body')
+    list_display = ('id', 'title', 'body','posted_at', 'published_at', 'like')
 
     # django-import-exportsの設定
     resource_class = ArticleResource
+
+@admin.register(Comment)
+class CommentAdmin(ImportExportModelAdmin):
+    # ImportExportModelAdminを利用するようにする
+    ordering = ['id']
+    list_display = ('id', 'text','posted_at','article')
+
+    # django-import-exportsの設定
+    resource_class = CommentResource
