994934d0502157023f5894fa3e5923b8b156977a
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-05-19 21:27:43 +0900

    admin export import

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cram'
+    'cram',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- cram/admin.py
+++ cram/admin.py
@@ -1,3 +1,21 @@
+# admin.py
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from .models import Article
+
+class ArticleResource(resources.ModelResource):
+    # Modelに対するdjango-import-exportの設定
+    class Meta:
+        model = Article
+
+
+@admin.register(Article)
+class ArticleAdmin(ImportExportModelAdmin):
+    # ImportExportModelAdminを利用するようにする
+    ordering = ['id']
+    list_display = ('id', 'title', 'body')
+
+    # django-import-exportsの設定
+    resource_class = ArticleResource
