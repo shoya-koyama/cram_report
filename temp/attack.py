66809b0c0c999cb11c03eec1ed9c6e46f18c6997
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-05-23 11:53:44 +0900

    Update attack.py

--- temp/attack.py
+++ temp/attack.py
@@ -1,11 +1,15 @@
 a = int(input())
 b = int(input())
 
-def attack(a,b):
-    for i in range(10**18):
-        a -=  b * i
-        if a < 0:
-            break
-    return i + 1
-    
-print(attack(a, b))
+#def attack(a,b):
+#    for i in range(10**18):
+#        a -=  b * i
+#        if a < 0:
+#            break
+#    return i + 1
+#    
+#print(attack(a, b)
+
+
+#a, b = map(int, input().split())
+print((a + b - 1) // b)
