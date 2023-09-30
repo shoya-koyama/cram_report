fe0ed69774915cd8e62bf19b1d06d969401422f5
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-09-30 23:00:49 +0900

    move myHobby

--- tcdr.py
+++
@@ -1,8 +0,0 @@
-s = input()
-
-result = ''
-for i in range(0, len(s)):
-    if (s[i] != 'a') and (s[i] != 'i') and (s[i] != 'u') and (s[i] != 'e') and (s[i] != 'o'):
-        result += s[i]
-
-print(result)
